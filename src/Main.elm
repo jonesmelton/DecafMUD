2dd0a333f1ddf7a6dfc6d0908aa223c2d89fda8e
Jones Melton <shavah@gmail.com>
2021-11-24 22:05:40 -0600

    First parse of ansi in elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 port module Main exposing (..)
 
+import Ansi
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -13,13 +14,12 @@ import Json.Decode as D
 
 main : Program () Model Msg
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
-
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -27,6 +27,8 @@ main =
 
 
 port sendMessage : String -> Cmd msg
+
+
 port sendToElm : (String -> msg) -> Sub msg
 
 
@@ -35,16 +37,20 @@ port sendToElm : (String -> msg) -> Sub msg
 
 
 type alias Model =
-  { draft : String
-  , messages : List String
-  }
+    { draft : String
+    , messages : List String
+    , mudlines : List Ansi.Action
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init flags =
-  ( { draft = "", messages = [] }
-  , Cmd.none
-  )
+    ( { draft = ""
+      , messages = []
+      , mudlines = []
+      }
+    , Cmd.none
+    )
 
 
 
@@ -52,45 +58,51 @@ init flags =
 
 
 type Msg
-  = DraftChanged String
-  | Send
-  | Recv String
+    = DraftChanged String
+    | Send
+    | Recv String
+    | Mudline String
+
 
 
 -- Use the `sendMessage` port when someone presses ENTER or clicks
 -- the "Send" button. Check out index.html to see the corresponding
 -- JS where this is piped into a WebSocket.
 --
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    DraftChanged draft ->
-      ( { model | draft = draft }
-      , Cmd.none
-      )
+    case msg of
+        DraftChanged draft ->
+            ( { model | draft = draft }
+            , Cmd.none
+            )
 
-    Send ->
-      ( { model | draft = "" }
-      , sendMessage model.draft
-      )
+        Send ->
+            ( { model | draft = "" }
+            , sendMessage model.draft
+            )
 
-    Recv message ->
-      ( { model | messages = model.messages ++ [message] }
-      , Cmd.none
-      )
+        Recv message ->
+            ( { model | messages = model.messages ++ [ message ] }
+            , Cmd.none
+            )
+
+        Mudline line ->
+            Debug.log "elm logging"
+                ( { model | mudlines = Ansi.parse line ++ model.mudlines }
+                , Cmd.none
+                )
 
 
 
--- SUBSCRIPTIONS
+-- coming in from JS side
 
 
--- Subscribe to the `messageReceiver` port to hear about messages coming in
--- from JS. Check out the index.html file to see how this is hooked up to a
--- WebSocket.
---
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  sendToElm Recv
+    sendToElm Mudline
 
 
 
@@ -99,20 +111,20 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text "Echo Chat" ]
-    , ul []
-        (List.map (\msg -> li [] [ text msg ]) model.messages)
-    , input
-        [ type_ "text"
-        , placeholder "Draft"
-        , onInput DraftChanged
-        , on "keydown" (ifIsEnter Send)
-        , value model.draft
+    div []
+        [ h1 [] [ text "Echo Chat" ]
+        , ul []
+            (List.map (\msg -> li [] [ text msg ]) model.messages)
+        , input
+            [ type_ "text"
+            , placeholder "Draft"
+            , onInput DraftChanged
+            , on "keydown" (ifIsEnter Send)
+            , value model.draft
+            ]
+            []
+        , button [ onClick Send ] [ text "Send" ]
         ]
-        []
-    , button [ onClick Send ] [ text "Send" ]
-    ]
 
 
 
@@ -121,5 +133,12 @@ view model =
 
 ifIsEnter : msg -> D.Decoder msg
 ifIsEnter msg =
-  D.field "key" D.string
-    |> D.andThen (\key -> if key == "Enter" then D.succeed msg else D.fail "some other key")
+    D.field "key" D.string
+        |> D.andThen
+            (\key ->
+                if key == "Enter" then
+                    D.succeed msg
+
+                else
+                    D.fail "some other key"
+            )
